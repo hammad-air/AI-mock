43e6888182afcc4cdf30daf94a90b61166b88230
hammad-air <hammadn788@gmail.com>
2024-08-29 19:14:37 +0500

    new added commit

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
   <div>
-    <h1>hello world</h1>
+    <h1>hello Karachi</h1>
     <Button>Subscribe</Button>
 
   </div>
--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://AI-Interview_owner:b8jMZOrFCmA5@ep-lucky-pond-a1f1ha95.ap-southeast-1.aws.neon.tech/AI-Interview?sslmode=require',
+      url: "postgresql://interviewdb_owner:pgYSyfV6xe9P@ep-royal-salad-a55hclil.us-east-2.aws.neon.tech/interviewdb?sslmode=require",
     }
   };
